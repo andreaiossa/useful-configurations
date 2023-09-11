863a6f1483db34931e7e0c8a0538b643c0cf28e4
andrea iossa <andreaiossa97@gmail.com>
2023-09-11 13:51:53 +0200

    [fish] updated config file

--- fish/config.fish
+++ fish/config.fish
@@ -1,25 +1,20 @@
-
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-__check_nvm
-
+# <<< ITERM >>>
 source ~/.iterm2_shell_integration.fish
+
+# <<< OHMYPOSH >>>
 oh-my-posh init fish --config ~/mytheme.omp.json | source
 
-
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
+# <<< CONDA >>>
 eval /usr/local/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
 
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/yoza/google-cloud-sdk/path.fish.inc' ]; . '/Users/yoza/google-cloud-sdk/path.fish.inc'; end
-
+# <<< KUBE >>>
+if [ -f '/Users/yoza/google-cloud-sdk/path.fish.inc' ]; . '/Users/yoza/google-cloud-sdk/path.fish.inc'; end # updates PATH for the Google Cloud SDK.
 export USE_GKE_GCLOUD_AUTH_PLUGIN=True
 alias k="kubectl"
-
 kubectl completion fish | source
+
+# <<< GLOBAL VARIABLES >>>
+set -U NUCLEUS /Users/yoza/Desktop/nucleus
+
+# <<< NVM >>>
+__check_nvm
