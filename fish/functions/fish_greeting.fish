c61ca473bf80dfc77d079f45bce05fb2652ed31b
andrea iossa <andreaiossa97@gmail.com>
2023-09-12 15:45:38 +0200

    added new fish greating message

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,3 +1,9 @@
 function fish_greeting
-  echo Starting ﾁ讠丂卄 
+  set -l json_file (curl -s "https://api.quotable.io/random")
+  set -l content (echo $json_file | jp content)
+  set -l author (echo $json_file | jp author)
+
+  set_color cyan; echo $content
+  set_color cyan; echo $author.
+
 end
