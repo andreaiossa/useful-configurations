f7e336cf714991eed88b0ced5084ded871a6740f
andrea iossa <andreaiossa97@gmail.com>
2023-10-06 15:37:03 +0200

    [FISH] changed config file. Added configs related to podman in fish and others docker-alternatives settings

--- fish/config.fish
+++ fish/config.fish
@@ -9,9 +9,20 @@ eval /usr/local/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" $
 
 # <<< KUBE >>>
 if [ -f '/Users/yoza/google-cloud-sdk/path.fish.inc' ]; . '/Users/yoza/google-cloud-sdk/path.fish.inc'; end # updates PATH for the Google Cloud SDK.
-export USE_GKE_GCLOUD_AUTH_PLUGIN=True
+#export USE_GKE_GCLOUD_AUTH_PLUGIN=True
+set -x -U USE_GKE_GCLOUD_AUTH_PLUGIN True
+
+# <<< Aliases >>>
 alias k="kubectl"
+alias pm="podman"
+alias pmm="podman machine"
+
+
+# <<< Completions >>>
 kubectl completion fish | source
+k3d completion fish | source
+podman completion fish | source
+kind completion fish | source
 
 # <<< GLOBAL VARIABLES >>>
 set -x -U NUCLEUS /Users/yoza/Desktop/nucleus
@@ -21,5 +32,4 @@ __check_nvm
 
 # <<< dock-tales >>>
 set --export --prepend PATH "/Users/yoza/.rd/bin" # monster rancher desktop
-alias pm="podman"
-alias pmm="podman machine"
+set -x -U DOCKER_HOST "unix:///Users/yoza/.local/share/containers/podman/machine/qemu/podman.sock"
