62624166256546164df857509a707bb2cf93948a
yoza <andreaiossa97@gmail.com>
2024-01-10 15:51:43 +0100

    [Fish] added iTerm themes

--- fish/config.fish
+++ fish/config.fish
@@ -22,7 +22,7 @@ alias pmm="podman machine"
 
 # <<< GLOBAL VARIABLES >>>
 set -x -U NUCLEUS /Users/yoza/Desktop/projects/nucleus
-set -x -U DOCKER_HOST "unix:///Users/yoza/.local/share/containers/podman/machine/qemu/podman.sock"
+# set -x -U DOCKER_HOST "unix:///Users/yoza/.local/share/containers/podman/machine/qemu/podman.sock"
 set -U run_variables_file_path "$HOME/.config/fish/run_variables"
 set -x -U GOPRIVATE "github.com/andreaiossa"
 
